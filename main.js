57f0d883cbd280c2f6ceb25d97d1afca55c619bc
Axel Bintoro <65143720+centralomd@users.noreply.github.com>
2020-10-24 14:14:03 +0700

    next

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ const app = express();
 app.use(express.static(__dirname))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/foo.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(3000, () => {
-  console.log('server started');
+  console.log('Server Launched!');
 });
